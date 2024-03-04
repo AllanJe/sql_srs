08a43d3d96ff272cd6a85557d3871652a58f84e8
Allan Jerolon <jerolona@gmail.com>
2024-03-04 16:42:14 +0100

    Revert "add_sidebar"

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ import pandas as pd
 import duckdb
 
 st.write("""
-# SQL SRS
+SQL SRS
 Spaced Repetition System SQL practice""")
 
-with st.sidebar:
-    option = st.selectbox(
-        'What would you like to review ?',
-        ('Joins', 'GroupBy', 'Windows Functions'),
-        index=None,
-        placeholder="Select a theme")
-    st.write(f"You selected : {option}")
+option = st.selectbox(
+    'What would you like to review ?',
+    ('Joins', 'GroupBy', 'Windows Functions'),
+    index=None,
+    placeholder="Select a theme")
+
+st.write(f"You selected : {option}")
 
 data = {"a": [1, 2, 3],
         "b": [4, 5, 6]}
