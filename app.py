01c55e3b17f31291b5f0c6a3f62910cbe45fec9d
AllanJe <jerolona@gmail.com>
2024-04-08 11:21:14 +0200

    remove theme non available and simplify code

--- app.py
+++ app.py
@@ -2,8 +2,17 @@ import contextlib
 import io
 import pandas as pd
 import ast
+import os
 import duckdb
 import streamlit as st
+
+if "data" not in os.listdir():
+    logging.error(os.listdir())
+    logging.error("creating folder data")
+    os.mkdir("data")
+if "exercises_sql_tables.duckdb" not in os.listdir("data"):
+    exec(open("init_db.py").read())
+    # subprocess.run(["python","init_db.py"])
 
 con = duckdb.connect(database="data/exercices_sql_tables.duckdb", read_only=False)
 st.write(
@@ -13,27 +22,34 @@ Spaced Repetition System SQL practice"""
 )
 
 with st.sidebar:
+    # available_themes_df = con_execute("SELECT DISTINCT theme FROM memory_state").df()
     theme = st.selectbox(
         "What would you like to review ?",
-        ("Cross Joins", "GroupBy", "Windows Functions"),
+        con.execute("SELECT DISTINCT theme FROM memory_state").df(), # available_themes_df["theme].unique()
+        #("Cross Joins", "GroupBy", "Windows Functions"),
         index=None,
         placeholder="Select a theme",
     )
-    st.write(f"You selected : {theme}")
     if theme:
-        exercise = (
-            con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'")
-            .df()
-            .sort_values("last_reviewed")
-            .reset_index()
-        )
-        st.write(exercise)
+        st.write(f"You selected : {theme}")
+        select_exercise_query = f"SELECT * FROM memory_state WHERE theme = '{theme}'"
+    else:
+        select_exercise_query = f"SELECT * FROM memory_state"
 
-        exercise_name = exercise.loc[0, "exercise_name"]
-        with open(f"answers/{exercise_name}.sql", "r") as f:
-            answer = f.read()
+    exercise = (
+        con.execute(select_exercise_query)
+        .df()
+        .sort_values("last_reviewed")
+        .reset_index(drop=True)
+    )
 
-        solution_df = con.execute(answer).df()
+    st.write(exercise)
+
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
+
+    solution_df = con.execute(answer).df()
 # answer_str = """
 # SELECT * FROM beverages
 # CROSS JOIN food_items
